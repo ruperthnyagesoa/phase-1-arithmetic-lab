fc1a05241cf1e42d21b472c0b33a407a24309f2a
Ruperth <ruperthnyagesoa@gmail.com>
2023-03-13 12:05:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 1 
+const num2 = 62
+let num3 = 29
+let num4 = 5
+let mod =(num3%= num4)
+const multiply= num1 * num2
+
+const random= 2
+let max = 20
+max.math(0,20)
+//let mod = (num3 %= num4)
